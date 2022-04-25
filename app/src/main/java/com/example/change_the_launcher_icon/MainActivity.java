1470e858039271f6e7bd80e82d359531ca3accd0
vuong <vuong.thcodeforces194412@outlook.com.vn>
2022-04-25 17:01:55 +0700

    task1 1.4

---
+++ app/src/main/java/com/example/change_the_launcher_icon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.change_the_launcher_icon;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
